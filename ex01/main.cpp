50ede25b74253aeae01df8cf54447e91668b61f2
besalort <benjamin.salort@hotmail.com>
2024-04-22 19:43:28 +0200

    ex01 fini

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: besalort <besalort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:13:20 by besalort          #+#    #+#             */
-/*   Updated: 2024/04/19 18:27:00 by besalort         ###   ########.fr       */
+/*   Updated: 2024/04/22 19:42:57 by besalort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ int	main() {
 	for (int i = 0; i < nb ; i++)
 	{
 		Horde[i].announce();
-		// Horde->~Zombie();
 	}
-	delete Horde[i];
+	delete[] Horde;
 	return (0);
 }
